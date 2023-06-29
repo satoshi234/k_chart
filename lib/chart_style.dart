dcce6487231cf3b85e5650e71c8e5902d71329d4
niki.satoshi <niki.satoshi@toiware.co.jp>
2023-06-30 02:01:06 +0900

    コメント追記: chart_style

--- lib/chart_style.dart
+++ lib/chart_style.dart
@@ -28,17 +28,13 @@ class ChartColors {
   Color nowPriceDnColor = Color(0xffC15466);
   Color nowPriceTextColor = Color(0xffffffff);
 
-  //深度颜色
   Color depthBuyColor = Color(0xff60A893);
   Color depthSellColor = Color(0xffC15866);
 
-  //选中后显示值边框颜色
   Color selectBorderColor = Color(0xff6C7A86);
 
-  //选中后显示值背景的填充颜色
   Color selectFillColor = Color(0xff0D1722);
 
-  //分割线颜色
   Color gridColor = Color(0xff4c5c74);
 
   Color infoWindowNormalColor = Color(0xffffffff);
@@ -50,7 +46,6 @@ class ChartColors {
   Color vCrossColor = Color(0x1Effffff);
   Color crossTextColor = Color(0xffffffff);
 
-  //当前显示内最大和最小值的颜色
   Color maxColor = Color(0xffffffff);
   Color minColor = Color(0xffffffff);
 
@@ -67,46 +62,53 @@ class ChartColors {
 }
 
 class ChartStyle {
+  /// チャート群の描画領域の上のパディング
   double topPadding = 30.0;
 
+  /// チャート群の描画領域の下のパディング
+  /// * ここに日付が表示する
   double bottomPadding = 20.0;
 
+  /// ひとつ上のチャートとの間隔
   double childPadding = 12.0;
 
-  //点与点的距离
+  // 各データの間隔
   double pointWidth = 11.0;
 
-  //蜡烛宽度
+  /// ロウソク足の幅
   double candleWidth = 8.5;
 
-  //蜡烛中间线的宽度
+  /// ロウソク足の線の幅
   double candleLineWidth = 1.5;
 
-  //vol柱子宽度
+  /// volバーの幅
   double volWidth = 8.5;
 
-  //macd柱子宽度
+  /// macdバーの幅
   double macdWidth = 3.0;
 
-  //垂直交叉线宽度
+  /// 十字線の縦線の幅
   double vCrossWidth = 8.5;
 
-  //水平交叉线宽度
+  /// 十字線の横線の幅
   double hCrossWidth = 0.5;
 
-  //现在价格的线条长度
-  double nowPriceLineLength = 1;
+  /// 現在価格の破線の線分の長さ
+  double nowPriceLineLength = 5;
 
-  //现在价格的线条间隔
+  /// 現在価格の破線の間隔
   double nowPriceLineSpan = 1;
 
-  //现在价格的线条粗细
+  /// 現在価格の破線の幅
   double nowPriceLineWidth = 1;
 
+  /// チャートのグリッドの行数
   int gridRows = 4;
 
-  int gridColumns = 4;
+  /// チャートのグリッドの列数
+  int gridColumns = 5;
 
-  //下方時間客製化
+  /// 日付のフォーマット文字列
+  /// ex) yyyy-MM-dd HH:mm
   List<String>? dateTimeFormat;
 }
